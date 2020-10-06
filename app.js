aca598556a4630ebe5f3943f31d075eb4c9dc839
abWasHere <audrey.belson@gmail.com>
2020-10-07 00:32:49 +0200

    finish the app

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require("dotenv").config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
+// frontend access to server
+const cors = require("cors");
+const corsOptions = { origin: process.env.FRONTEND_URL, credentials: true };
+app.use(cors(corsOptions));
+
+// base config
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var fs = require("fs"); //to manipulate data in JSON file
+var shortid = require("shortid"); //get random ids
+
+var data = fs.readFileSync("./tasks.json"); 
+var todos = JSON.parse(data); // JSON to array of obj
+
+//  ---------  routes prefix = /api  -----------
+
+/* GET ALL */
+//
+router.get("/", (req, res) => {
+	if (todos.length) {
+		res.status(200).json(todos);
+	} else {
+		res.status(500).json(err);
+	}
 });
+
+/* CREATE */
+//
+router.post("/add", (req, res) => {
+	if (req.body.task) {
+		let newTask = { task: req.body.task, _id: shortid.generate() };
+
+		todos.push(newTask);
+
+		let datas = JSON.stringify(todos, null, 2); // array to JSON
+		fs.writeFileSync("tasks.json", datas, () => {
+			console.log("error");
+		}); // save new JSON with new data
+
+		res.status(201).json(newTask);
+	} else {
+		res.status(500).json({message:"Invalid content"});
+	}
+});
+
+/* DELETE */
+//
+router.delete("/delete/:id", (req, res) => {
+	let cleanArr = todos.filter((x) => x._id != req.params.id);
+	let cleanData = JSON.stringify(cleanArr, null, 2);
+	fs.writeFileSync("tasks.json", cleanData, () => {
+		console.log("error");
+	});
+	res.sendStatus(204);
+});
+
 
 module.exports = router;
